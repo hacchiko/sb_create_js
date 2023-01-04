549f8124b04aabc1218c580f80c800ce29604b20
hacchiko <valkyria.zer0256@gmail.com>
2023-01-04 12:34:03 +0900

    whileでゲーム作成-1

---
+++ step3/3-07/index.js
@@ -0,0 +1,22 @@
+// console.log('Helloworld')
+
+
+
+// 攻撃の数値だけダメージ与える
+// モンスターの攻撃が０以下になるまで繰り返す
+
+// 体力１００のモンスター
+let enemy = 100;
+let attack;
+window.alert('戦闘スタート');
+
+// モンスターの攻撃が０以下になるまで繰り返す
+while(enemy > 0){
+  // 攻撃一回につき３０以下、毎回ランダム
+  attack = Math.floor(Math.random() * 30)+1;
+  console.log('モンスターに'+ attack + 'のダメージ');
+  // ダメージ換算
+  enemy = enemy - attack;
+}
+
+console.log('モンスターを倒した！！')
